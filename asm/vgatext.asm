896ec3ef36a821a5bb475f0acfd49a504f73a63f
Lawrence Manning <lawrence@aslak.net>
2020-09-16 15:16:56 +0100

    Added a PS2 demo program, and tweaked the VGA demo
    PS2 demo reads PS2 port on my devboard and outputs keypresses etc.

--- asm/vgatext.asm
+++ asm/vgatext.asm
@@ -2,13 +2,12 @@ start:		load.w r0,#0xf00f	; canary
 		load.w r1,#0x8000	; start of video memory
 		load.w r7,#0x80		; stack pointer
 
-loop:		load.bu r4,#0x0f
+loop:		load.bu r4,#0x0f	; polarity of text
 		load.w r2,#0x4000	; delay
-delay:		load.w r3,#0x500
-		load.w r3,(r3)
-		test r3
-		branchnz notpushed
-		load.bu r4,#0xf0
+delay:		load.w r3,0x500		; get button state
+		test r3			; see if we pushed
+		branchnz notpushed	; yes/no
+		load.bu r4,#0xf0	; yes: black on white
 notpushed:	dec r2			; dec delay
 		jumpnz delay		; more delay
 		calljump printmessage	; call sub
@@ -23,9 +22,9 @@ messageloop:	load.bu r3,(r2)		; get this letter in r3
 		inc r2			; next letter
 		store.w (r1),r3		; write letter
 		incd r1			; inc video pointer
-		store.w 0x200,r1	; put the address-8000 on the lcd
+		store.w 0x200,r1	; put the address on the lcd
 		branch messageloop	; back to the next letter
 printmessageo:	return
 
-message:	#str "Hello, World! 123456 \0"
+message:	#str "[ Hello, World! ]\0"
 
